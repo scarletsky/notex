67b44e0a3f8f394c440a12b3e76e72f721edd1a4
scarletsky <scarletsky1025@gmail.com>
2017-04-21 23:49:17 +0800

    feat: check authorization for notes

--- web/views/note_view.ex
+++ web/views/note_view.ex
@@ -2,10 +2,14 @@ defmodule Notex.NoteView do
   use Notex.Web, :view
 
   def render("note.json", note) do
-      %{id: note.id}
+    %{id: note.id}
   end
 
   def render("error.json", note) do
-      %{error: "errrrrrrr"}
+    %{error: "errrrrrrr"}
+  end
+
+  def is_owner(conn, note) do
+    Plug.Conn.get_session(conn, :current_user) == note.creator_id
   end
 end
