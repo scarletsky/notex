64a511b159df60e0e4e297fed1a3f97ded35b334
scarletsky <scarletsky1025@gmail.com>
2017-05-23 08:51:10 +0800

    feat: add tags in notes list page

--- web/controllers/note_controller.ex
+++ web/controllers/note_controller.ex
@@ -9,7 +9,9 @@ defmodule Notex.NoteController do
   plug Notex.Plug.Authenticate when action in [:new, :create, :edit, :update, :delete]
 
   def index(conn, _params) do
-    notes = Repo.all(Note) |> Repo.preload(:creator)
+    notes = Repo.all(Note)
+      |> Repo.preload(:creator)
+      |> Repo.preload(:tags)
     render(conn, "index.html", notes: notes)
   end
 
