b032b9656305740d97e43051978ea18355db6eb2
scarletsky <scarletsky1025@gmail.com>
2017-06-11 17:00:42 +0800

    deploy: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+# Compile assets
+# brunch build --production
+npm install
+npm run deploy
+MIX_ENV=prod mix phoenix.digest
+
+# Custom tasks (like DB migrations)
+MIX_ENV=prod mix ecto.migrate
+
+# Finally run the server
+PORT=4001 MIX_ENV=prod mix phoenix.server
